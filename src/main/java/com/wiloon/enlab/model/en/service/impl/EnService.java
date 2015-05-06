9bb8b9c44be4452c9c7b850dfcaf136fb3960586
wiloon <wiloon.wy@gmail.com>
2015-05-06 21:51:43 +0800

    merge controller

--- src/main/java/com/wiloon/enlab/model/en/service/impl/EnService.java
+++ src/main/java/com/wiloon/enlab/model/en/service/impl/EnService.java
@@ -14,6 +14,7 @@ import com.wiloon.enlab.model.en.service.IEnService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class EnService extends GenericService<ECP, Long> implements IEnService {
     /**
      * Logger for this class
